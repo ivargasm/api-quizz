75a5568150a18a4b1f91ddca2d56ae7992d65ee3
ivargasm <ivargasm@hotmail.com>
2023-09-07 09:53:06 -0600

    debug

--- models/conection.php
+++ models/conection.php
@@ -4,14 +4,15 @@
 
         static public function conectar(){
 
-            $DB_HOST = getenv('DB_HOST');
+            // $DB_HOST = getenv('DB_HOST');
+            $DB_HOST = $_ENV['DB_HOST'];
             $DB_NAME = getenv('DB_NAME');
             $DB_USER = getenv('DB_USER');
             $DB_PASS = getenv('DB_PASS');  // Si has definido la variable de entorno, descomenta esta línea
             // $DB_PASS = "";
             $DB_PORT = getenv('DB_PORT');
 
-            print_r($_ENV['DB_HOST']);
+            print_r($DB_HOST);
             print_r($DB_NAME);
             print_r($DB_USER);
             print_r($DB_PASS);
