f7d7d1a203e34f890d2a5b8f45d08f8bb06f1c76
ivargasm <ivargasm@hotmail.com>
2023-09-07 09:05:50 -0600

    debug

--- models/conection.php
+++ models/conection.php
@@ -11,6 +11,12 @@
             // $DB_PASS = "";
             $DB_PORT = getenv('DB_PORT');
 
+            print_r($DB_HOST);
+            print_r($DB_NAME);
+            print_r($DB_USER);
+            print_r($DB_PASS);
+            print_r($DB_PORT);
+
 
             
             $link = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME;port=$DB_PORT",
