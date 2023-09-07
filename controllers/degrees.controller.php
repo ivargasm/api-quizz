6c4c8cbdf4d954930fc11e9fb650871335b33c8a
ivargasm <ivargasm@hotmail.com>
2023-09-07 09:08:37 -0600

    debug

--- controllers/degrees.controller.php
+++ controllers/degrees.controller.php
@@ -3,6 +3,7 @@
     class DegreesController{
 
         public function index(){
+            print_r("entre a degrees controller");
 
             // extraer los datos del modelo y enviarlos a la vista
             $degrees = DegreesModel::getDegrees();
