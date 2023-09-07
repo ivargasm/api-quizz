7af24b5957f2cf5432570040f74f1a372acf3a93
ivargasm <ivargasm@hotmail.com>
2023-09-07 11:11:09 -0600

    debug

--- models/conection.php
+++ models/conection.php
@@ -1,6 +1,10 @@
 <?php
 
-    $rootDirectory = $_SERVER['DOCUMENT_ROOT'] . '/api-quizz/';
+    if ($_SERVER['HTTP_HOST'] === 'localhost' || $_SERVER['HTTP_HOST'] === '127.0.0.1') {
+        $rootDirectory = $_SERVER['DOCUMENT_ROOT'] . '/api-quizz/';
+    } else {
+        $rootDirectory = $_SERVER['DOCUMENT_ROOT'];
+        
     require $rootDirectory . 'vendor/autoload.php';  // Asegúrate de incluir el autoload de Composer al inicio de tu script
 
     // Carga las variables de entorno desde .env
