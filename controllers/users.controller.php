d556c7deaaffe3b4a451fac49dc2992cc1b818d4
ivargasm <ivargasm@hotmail.com>
2023-09-16 15:12:26 -0600

    ajustes api users

--- controllers/users.controller.php
+++ controllers/users.controller.php
@@ -3,7 +3,7 @@
     class UsersController{
         public function index($data){
 
-            $data_users = $data[3];
+            $data_users = $data[4];
             // extraer datos del modelo y enviarlos a la vista
             $users = UsersModel::getUsers($data_users);
 
