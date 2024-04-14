04ea962c0c937a2141e3befe2179cfcd345119bd
isvargasm <ismael.vargas@storecheck.com>
2024-04-14 11:03:35 -0600

    update query questions para agregar columna is_open y active

--- models/questions.model.php
+++ models/questions.model.php
@@ -18,7 +18,8 @@
                         q.description AS 'question',
                         q.code,
                         GROUP_CONCAT(a.description ORDER BY a.random_order SEPARATOR '|') AS 'answers',
-                        MAX(CASE WHEN a.correct_answer = 1 THEN a.ans_number ELSE 0 END) - 1 AS 'correctAnswer'
+                        MAX(CASE WHEN a.correct_answer = 1 THEN a.ans_number ELSE 0 END) - 1 AS 'correctAnswer',
+                        q.is_open
                     FROM
                         (
                             SELECT *,
@@ -33,7 +34,7 @@
                                 ) as rand_ordered_answers,
                                 (SELECT @row_number:=0, @prev_question_id:=NULL) r
                         ) a
-                    JOIN questions q ON q.id = a.question_id
+                    JOIN questions q ON q.id = a.question_id and active is not false
                     join degrees d on d.id = q.degree_id and degree_id = :degree
                     join topics t on t.id = q.topic_id and topic_id = :topic
                     join partners s on s.id = q.partner_id and q.partner_id = :user
@@ -52,7 +53,8 @@
                         q.description AS 'question',
                         q.code,
                         GROUP_CONCAT(a.description ORDER BY a.random_order SEPARATOR '|') AS 'answers',
-                        MAX(CASE WHEN a.correct_answer = 1 THEN a.ans_number ELSE 0 END) - 1 AS 'correctAnswer'
+                        MAX(CASE WHEN a.correct_answer = 1 THEN a.ans_number ELSE 0 END) - 1 AS 'correctAnswer',
+                        q.is_open
                     FROM
                         (
                             SELECT *,
@@ -67,7 +69,7 @@
                                 ) as rand_ordered_answers,
                                 (SELECT @row_number:=0, @prev_question_id:=NULL) r
                         ) a
-                    JOIN questions q ON q.id = a.question_id
+                    JOIN questions q ON q.id = a.question_id and active is not false
                     join degrees d on d.id = q.degree_id and degree_id = :degree
                     join topics t on t.id = q.topic_id and topic_id = :topic
                     join partners s on s.id = q.partner_id and q.partner_id = :user
